6f0739a5127a799796b5c3ca5bff6c2a89e8b5d7
kimdooh <johnkdo2020@gmail.com>
2021-03-15 17:42:43 +0900

    결제 요청 update API는 구현 x b/c 비지니스 프로세스 취소 => 재결재

--- app/payments/urls.py
+++ app/payments/urls.py
@@ -5,4 +5,8 @@ from payments.views import PaymentListAPIView, PaymentCreateAPIView
 urlpatterns = [
     path('request/list/', PaymentListAPIView.as_view()),
     path('request/create/', PaymentCreateAPIView.as_view()),
+    # 결제 부분은 업데이트 없는것으로
+    # 비지니스 프로세스가 취소 -> 다시 결제
+    path('request/update/',),
+
 ]
